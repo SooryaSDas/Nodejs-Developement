e92c184691276d9ee4c1f0c0ad52e85d4d5b3033
SooryaSDas <sooryasandra6@gmail.com>
2023-05-25 15:18:12 +0530

    hbs

--- EXPRESS_EXAMPLES/routes/about.js
+++ EXPRESS_EXAMPLES/routes/about.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('about', {title :'node project', project : 'node'});
+
+  const values = ["soorya","sandra","sreeja","lohidas","sooraj"];
+
+  // const person = {name : "soorya",comments:{comment:"this is a comment from person.comments"}}
+
+  const person = {name : "soorya",ad:{admin:false}}
+
+
+  res.render('about', {person});
 });
 
 module.exports = router;
