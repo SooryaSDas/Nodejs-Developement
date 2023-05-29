c8c60066861f489d77687ad1dbb938009a29e6a4
SooryaSDas <sooryasandra6@gmail.com>
2023-05-29 12:25:04 +0530

    db connection

--- EXPRESS_EXAMPLES/routes/index.js
+++ EXPRESS_EXAMPLES/routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Soorya development', project : "node" });
 });
+
+router.post('/submit',function(req,res){
+  console.log(req.body)
+
+  MongoClient.connect('mongodb://localhost:27017/',function(err,client){
+    if(err)
+      console.log("errror");
+
+    else{
+      console.log("yes.")
+      client.db('crossroads').collection('users').insertOne(req.body);
+    }
+  })
+  console.log("below db")
+
+  res.send("got it")
+})
 
 module.exports = router;
